f8e935bb40ac164ff603e1860e277ef288b61fb9
Ashutosh Dikshit <adikshit@akamai.com>
2024-02-22 19:43:40 +0530

    Working backend includes transactions automacity

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const jwt = require("jsonwebtoken")
 const {JWT_SECRET} = require("../config")
 const { Account } = require("../db")
+const { default: mongoose } = require("mongoose")
 
 
 
@@ -31,22 +32,36 @@ router.get("/balance",authMiddleware,async (req,res)=>{
 })
 
 router.post("/transfer", authMiddleware, async(req,res)=>{
+    
+    // Starts the sessions it makes sure only one transaction takes place at a time
+    const session = await mongoose.startSession()
+    session.startTransaction() 
     const {amount, sendTo} = req.body
-
+    if(amount == 0){
+        await session.abortTransaction()
+        return res.status(400).json({
+            msg:"Amount cant be zero"
+        })
+    }
     const account = await Account.findOne({
         userId:req.userId
-    })
-    if(account.balance<amount){
-        return res.status(400).send({
+    }).session(session)
+
+    if(!account || account.balance<amount){
+        
+        await session.abortTransaction()
+        return res.status(400).json({
             msg:"Insufficient balance"
         })
     }
 
     const toAccount = await Account.findOne({
         userId:sendTo
-    })
+    }).session(session)
+
     if(!toAccount){
-        return res.status(400).send({
+        await session.abortTransaction()
+        return res.status(400).json({
             msg: "Invalid account"
         })
     }
@@ -56,7 +71,7 @@ router.post("/transfer", authMiddleware, async(req,res)=>{
         $inc:{
             balance:-amount
         }
-    })
+    }).session(session)
 
     await Account.updateOne({
         userId:sendTo
@@ -64,12 +79,13 @@ router.post("/transfer", authMiddleware, async(req,res)=>{
         $inc:{
             balance:+amount
         }
-    })
+    }).session(session)
+    // Commits the transaction once both the update is run successfully.
+    await session.commitTransaction()
     res.json({
         msg:"Amount transfered"
-        
+
     })
-    
 })
 
 
