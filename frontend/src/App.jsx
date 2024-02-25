7183dd7c22d85a9e95c4973e8059ffa4a92976c4
Ashutosh Dikshit <adikshit@akamai.com>
2024-02-25 17:36:24 +0530

    frontend Pages-Signup and Signin. Few reusable components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Dashboard } from './pages/Dashboard'
+import { SendMoney } from './pages/sendMoney'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import {Routes,BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     hi
-    </>
+  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="sendMoney" element={<SendMoney/>} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
---
+++ frontend/src/pages/sendMoney.jsx
@@ -0,0 +1,7 @@
+export const SendMoney = ()=>{
+
+    return <>
+
+        <h1>SendMoney</h1>
+    </>
+}
