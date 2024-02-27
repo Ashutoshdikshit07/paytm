2809370c6bd9540595d0439cdaed4f8c21800e65
Ashutosh Dikshit <adikshit@akamai.com>
2024-02-28 01:09:43 +0530

    #6 Dashboard balance fix

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,10 +1,28 @@
+import { useEffect,useState } from "react"
+import axios from "axios"
+
 export const Balance =({value})=>{
+    const [ balance,setBalance] = useState()
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            const response = await axios.get("http://localhost:3000/api/v1/account/balance",{
+                headers:{
+                    Authorization: "Bearer " + localStorage.getItem("token")
+                }
+            })
+        
+            setBalance(response.data.balance.toFixed(2))
+        }
+        
+        fetchData()
+    },[])
+    
     return <div className="flex">
         <div className="font-bold text-lg">
             Your Balance
         </div>
         <div className=" font-semibold text-lg ml-4">
-            Rs {value}
+            Rs {balance}
         </div>
     </div>
 }
